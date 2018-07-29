c417105e986f9a137d116940d392c9c7991fc362
Nagymester B <szabo.balazs13@gmail.com>
2018-07-29 17:38:06 +0200

    valami

---
+++ src/js/form-events.js
@@ -0,0 +1,7 @@
+window.addEventListener('resize', function(ev) {
+	console.debug('width', ev.target.innerWidth);
+});
+
+window.addEventListener('resize', function(ev) {
+	console.debug('height', ev.target.innerHeight);
+});
